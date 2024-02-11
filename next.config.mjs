3c830e7ee07f1a510c2badbf24d783bb8072a2ba
programmer-krish <s18002156@ousl.lk>
2024-02-11 15:17:19 +0530

    fixes added

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/",
+  basePath: "",
   images: {
-    remotePatterns: [
-      {
-        hostname: "images.pexels.com",
-      },
-      {
-        hostname: "media.licdn.com",
-      },
-    ],
+    unoptimized: true,
   },
   output: "export",
 };
